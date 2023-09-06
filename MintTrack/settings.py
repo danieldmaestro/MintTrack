884f40e39d59056f5dae586cbfb17c06030ab884
Daniel Momodu <momodufresh@gmail.com>
2023-09-06 13:57:01 +0100

    change debug

--- MintTrack/settings.py
+++ MintTrack/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = 'RENDER' not in os.environ
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = []
 
